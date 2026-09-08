ab56dee2db3628d1d246b25cbd5d80abe740e8f5
pramodyallapu <pramod.sai@amromed.org>
2026-09-08 23:51:59 +0530

    font

--- app/javascript/template_builder/builder.vue
+++ app/javascript/template_builder/builder.vue
@@ -1789,6 +1789,8 @@ export default {
 
       if (!field || !area) return
 
+      this.resetFieldFont(field)
+
       if (!isSameTemplate) {
         delete field.conditions
         delete field.preferences?.formula
@@ -1868,6 +1870,8 @@ export default {
         const field = JSON.parse(JSON.stringify(item.field))
         const area = JSON.parse(JSON.stringify(item.area))
 
+        this.resetFieldFont(field)
+
         if (!isSameTemplate) {
           delete field.conditions
           delete field.preferences?.formula
@@ -1922,6 +1926,13 @@ export default {
       this.selectedAreasRef.value = [...newAreas]
 
       this.save()
+    },
+    resetFieldFont (field) {
+      if (!field?.preferences) return
+
+      const fontKeys = ['font', 'font_size', 'font_type', 'align', 'valign', 'color']
+
+      fontKeys.forEach((key) => delete field.preferences[key])
     },
     hasClipboardData () {
       try {
